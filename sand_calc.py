24d78585429d04c7cdafabd2343bf8d2472c2f94
nirzaa <72485675+nirzaa@users.noreply.github.com>
2022-03-15 19:23:20 +0200

    updated

--- sand_calc.py
+++ sand_calc.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from scipy.io import loadmat
+import torch
 
 def rely():
     # path = './csv_files/class_2d_epochs_20energies/run_0/epoch_50'
@@ -34,7 +36,7 @@ def rel_error_table():
     rel_mean_list = list()
     rel_std_list = list()
     epoch_list = list()
-    my_path = os.path.join('csv_files', 'run_1')
+    my_path = os.path.join('csv_files', '2d_20classes', 'run_1')
     for i in np.linspace(10, 100, 10):
         if i.is_integer():
             i = int(i)
@@ -48,14 +50,66 @@ def rel_error_table():
         rel_std_list.append(rel_std)
     rel_df = pd.DataFrame(
     {'epoch': epoch_list,
-     'mean': rel_mean_list,
-     'std': rel_std_list
+     'mean[%]': rel_mean_list,
+     'std[%]': rel_std_list
     })
     rel_df.to_csv(os.path.join(my_path, 'rel.csv'))
+
+def rel_error_table_nonormal():
+    rel_mean_list = list()
+    rel_std_list = list()
+    epoch_list = list()
+    num_runs = 10
+    for run in range(num_runs):
+        my_path = os.path.join('csv_files', f'run_{run}')
+        for i in np.linspace(10, 100, 10):
+            if i.is_integer():
+                i = int(i)
+            print(f'Working on epoch_{i}')
+            df = pd.read_csv(os.path.join(my_path, f'epoch_{i}', 'data_frame.csv'))
+            target = df.target
+            output = df.output
+            rel_mean = ((target - output)/1).mean()
+            rel_std = ((target - output)/1).std()
+            epoch_list.append(i)
+            rel_mean_list.append(rel_mean)
+            rel_std_list.append(rel_std)
+        rel_df = pd.DataFrame(
+        {'epoch': epoch_list,
+        'mean': rel_mean_list,
+        'std': rel_std_list
+        })
+        rel_df.to_csv(os.path.join(my_path, 'rel_nonormal.csv'))
+        plt.figure(num=0, figsize=(12, 6))
+        plt.clf()
+        plt.title('error as a function of epoch')
+        plt.ylabel('rel error: target - output')
+        plt.xlabel('epoch')
+        plt.errorbar(epoch_list, rel_mean_list, yerr=rel_std_list)
+        plt.savefig(os.path.join(my_path, 'rel_nonormal'))
+  
+def show_noise():
+    noise_file = os.path.join('./', 'data', 'raw', 'fast.elaser_randomised_bg')
+    en_dep = loadmat(noise_file)['0']
+    en_dep_noise = torch.zeros((110, 11, 21))
+    for i in range(en_dep_noise.shape[0]):
+        for j in range(en_dep_noise.shape[1]):
+            for k in range(en_dep_noise.shape[2]):
+                en_dep_noise[i,j,k] = en_dep[k,i,j]
+    plt.figure(num=0, figsize=(12, 6))
+    plt.clf()
+    plt.imshow(en_dep_noise.sum(axis=1), interpolation="nearest", origin="upper", aspect="auto")
+    plt.colorbar()
+    plt.savefig('show_noise')
+    return None
 
 
 if __name__ == '__main__':
     # rely()
 
     # my_rel()
-    rel_error_table()
+    # rel_error_table()
+
+    # rel_error_table_nonormal()
+
+    show_noise()
