81feeaca242ee48080f92e834e74434a30c37976
nirzaa <72485675+nirzaa@users.noreply.github.com>
2022-03-17 07:32:39 +0200

    updated

--- sand_calc.py
+++ sand_calc.py
@@ -1,7 +1,10 @@
+from black import out
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from scipy.io import loadmat
+import torch
 
 def rely():
     # path = './csv_files/class_2d_epochs_20energies/run_0/epoch_50'
@@ -57,39 +60,78 @@ def rel_error_table_nonormal():
     rel_mean_list = list()
     rel_std_list = list()
     epoch_list = list()
-    my_path = os.path.join('csv_files', 'run_1')
-    for i in np.linspace(10, 100, 10):
-        if i.is_integer():
-            i = int(i)
-        print(f'Working on epoch_{i}')
-        df = pd.read_csv(os.path.join(my_path, f'epoch_{i}', 'data_frame.csv'))
-        target = df.target
-        output = df.output
-        rel_mean = ((target - output)/1).mean()
-        rel_std = ((target - output)/1).std()
-        epoch_list.append(i)
-        rel_mean_list.append(rel_mean)
-        rel_std_list.append(rel_std)
-    rel_df = pd.DataFrame(
-    {'epoch': epoch_list,
-     'mean': rel_mean_list,
-     'std': rel_std_list
-    })
-    rel_df.to_csv(os.path.join(my_path, 'rel_nonormal.csv'))
+    num_runs = 10
+    for run in range(num_runs):
+        my_path = os.path.join('csv_files', f'run_{run}')
+        for i in np.linspace(10, 100, 10):
+            if i.is_integer():
+                i = int(i)
+            print(f'Working on epoch_{i}')
+            df = pd.read_csv(os.path.join(my_path, f'epoch_{i}', 'data_frame.csv'))
+            target = df.target
+            output = df.output
+            rel_mean = ((target - output)/1).mean()
+            rel_std = ((target - output)/1).std()
+            epoch_list.append(i)
+            rel_mean_list.append(rel_mean)
+            rel_std_list.append(rel_std)
+        rel_df = pd.DataFrame(
+        {'epoch': epoch_list,
+        'mean': rel_mean_list,
+        'std': rel_std_list
+        })
+        rel_df.to_csv(os.path.join(my_path, 'rel_nonormal.csv'))
+        plt.figure(num=0, figsize=(12, 6))
+        plt.clf()
+        plt.title('error as a function of epoch')
+        plt.ylabel('rel error: target - output')
+        plt.xlabel('epoch')
+        plt.errorbar(epoch_list, rel_mean_list, yerr=rel_std_list)
+        plt.savefig(os.path.join(my_path, 'rel_nonormal'))
+  
+def show_noise():
+    noise_file = os.path.join('./', 'data', 'raw', 'fast.elaser_randomised_bg')
+    en_dep = loadmat(noise_file)['0']
+    en_dep_noise = torch.zeros((110, 11, 21))
+    for i in range(en_dep_noise.shape[0]):
+        for j in range(en_dep_noise.shape[1]):
+            for k in range(en_dep_noise.shape[2]):
+                en_dep_noise[i,j,k] = en_dep[k,i,j]
     plt.figure(num=0, figsize=(12, 6))
     plt.clf()
-    plt.title('error as a function of epoch')
-    plt.ylabel('rel error: target - output')
-    plt.xlabel('epoch')
-    plt.errorbar(epoch_list, rel_mean_list, yerr=rel_std_list)
-    plt.savefig(os.path.join(my_path, 'rel_nonormal'))
-  
+    plt.imshow(en_dep_noise.sum(axis=1), interpolation="nearest", origin="upper", aspect="auto")
+    plt.colorbar()
+    plt.savefig('show_noise')
+    return None
 
-
+def excel_maker():
+    run = 0
+    df_dict_output = dict()
+    df_dict_target = dict()
+    for run in [0,1,2]:
+        with open(os.path.join('saved','diff_run_res',f'bin_results_run_{run}.txt'), 'r') as f:
+            lines = f.readlines()
+            output = lines[0].split('[')[1].split()[:20]
+            output = [float(x[:-2]) for x in output]
+            target = lines[0].split('[')[2].split()[:20]
+            target = [float(x[:-2]) for x in target]
+            df_dict_output[f'{run}'] = output
+            df_dict_target[f'{run}'] = target
+        df_output = pd.DataFrame(data=df_dict_output)
+        df_target = pd.DataFrame(data=df_dict_target)
+    df_output.to_csv(os.path.join('csv_files', 'output.csv'))
+    df_target.to_csv(os.path.join('csv_files', 'target.csv'))
+    return None
 
 if __name__ == '__main__':
     # rely()
 
     # my_rel()
     # rel_error_table()
-    rel_error_table_nonormal()
+
+    # rel_error_table_nonormal()
+
+    # show_noise()
+
+    excel_maker()
+
